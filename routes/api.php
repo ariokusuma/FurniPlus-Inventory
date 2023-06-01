34a37ba9abca763a569236a66dfac8a4e8285940
ariokusuma <ariopuboyo@gmail.com>
2023-06-01 17:04:57 +0700

    API no3 and 4

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DatabaseBarangController;
 use App\Http\Controllers\DatabasePesananController;
+use App\Http\Controllers\DatabasePengemasanController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,16 +24,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 // Buat E-Commerce
-Route::GET('/data-barang', [DatabaseBarangController::class, 'index'])->name('dataBarang');
+Route::GET('/data-barang', [DatabaseBarangController::class, 'index'])->name('dataBarang'); // No 1
+Route::GET('/data-barang/{id_barang}', [DatabaseBarangController::class, 'showbyid'])->name('detilbarang'); // No 2
+Route::POST('/data-barang/add/', [DatabaseBarangController::class, 'store'])->name('detilbarang'); // No 3
+Route::PUT('/data-barang/update/{id}', [DatabaseBarangController::class, 'update'])->name('detilbarang'); // No 4
 
-Route::GET('/data-barang/{id_barang}', [DatabaseBarangController::class, 'showbyid'])->name('detilbarang');
+
+
 
 //endpoint nomor 7
 Route::apiResource('pesanan', DatabasePesananController::class);
 
-Route::PUT('pesanan/update/{id_pesanan}', [App\Http\Controllers\DatabasePesananController::class, 'update']);
+Route::PUT('pesanan/update/{id_pesanan}', [DatabasePesananController::class, 'update']);
 
 
 Route::GET('/data-pesanan/{id_pesanan}', [DatabasePesananController::class, 'showbyid'])->name('detilPesanan');
 
-Route::GET('pengiriman/kirim', [App\Http\Controllers\DatabasePengemasanController::class, 'index']);
+Route::GET('pengiriman/kirim', [DatabasePengemasanController::class, 'index']);

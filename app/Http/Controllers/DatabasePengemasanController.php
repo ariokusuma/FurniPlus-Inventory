2fc67938a580d3e1cbcf7f9e198ecb5b92fe42b1
yureyx <fachrifadhilahsanum22@gmail.com>
2023-06-03 22:14:43 +0700

    pengemasan id

--- app/Http/Controllers/DatabasePengemasanController.php
+++ app/Http/Controllers/DatabasePengemasanController.php
@@ -12,4 +12,12 @@ class DatabasePengemasanController extends Controller
         $pengemasan = DatabasePengemasan::where('status', 'siap dikirim dan diserahkan kepada pihak perngiriman')->get();
         return $pengemasan;
     }
+    public function pengiriman($id_pengemasan)
+    {
+        $id_pengemasan = DatabasePengemasan::findorfail($id_pengemasan);
+        if ($id_pengemasan) 
+        {
+            return response()->json($id_pengemasan);
+        }
+    }
 }

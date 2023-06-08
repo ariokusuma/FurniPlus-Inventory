fbcf753cac813527b99106ecede1643fb54c10fe
ariokusuma <ariopuboyo@gmail.com>
2023-06-08 10:31:12 +0700

    add

--- routes/api.php
+++ routes/api.php
@@ -42,6 +42,9 @@ Route::PUT('/data-barang/update/{id}', [DatabaseBarangController::class, 'update
 */
 //endpoint nomor 7
 Route::apiResource('pesanan/', DatabasePesananController::class); //No 5
+
+                        //No 6
+
 Route::PUT('pesanan/update/{id_pesanan}', [DatabasePesananController::class, 'update']); //No 7
 Route::GET('/data-pesanan/{id_pesanan}', [DatabasePesananController::class, 'showbyid'])->name('detilPesanan'); //No 7
 

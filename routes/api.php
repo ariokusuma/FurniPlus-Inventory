27dc932ddb772aa7675d3a4992483a122eb6830e
ariokusuma <ariopuboyo@gmail.com>
2023-06-14 21:13:43 +0700

    fixing api

--- routes/api.php
+++ routes/api.php
@@ -47,13 +47,13 @@ Route::DELETE('/data-barang/delete/{id}', [DatabaseBarangController::class, 'des
 Route::get('pesanan', [DatabasePesananController::class, 'fetch']); //fetch data
 Route::GET('pesanan/show', [DatabasePesananController::class, 'show']);
 Route::get('pesanan/show/{id}', [DatabasePesananController::class, 'showbyid']);
-Route::GET('updateStatus', [DatabasePesananController::class, 'updateStatus']); //
+Route::GET('updateStatus', [DatabasePesananController::class, 'updateStatus']); //resi
 
 
 // Route::GET('fill', [DatabasePesananController::class, 'fillNullData']);
 
 // update data resi
-Route::PUT('pesanan/update/{id_pesanan}', [DatabasePesananController::class, 'update']); //No 7
+// Route::PUT('pesanan/update/{id_pesanan}', [DatabasePesananController::class, 'update']); //No 7
 
 
 
